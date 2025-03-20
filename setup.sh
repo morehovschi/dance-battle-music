8fab4afd8db94db29b7995305a700dede71f6493
Marius Orehovschi <marius.orehovschi@gmail.com>
2025-03-20 17:09:27 +0100

    corrected annotations and added required packages in setup

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@ pip install playwright==1.50.0
 playwright install chromium
 pip install -U "yt-dlp[default]"
 pip install jupyter
+pip install numpy==2.1.3
 pip install pandas==2.2.3
+pip install librosa==0.11.0
